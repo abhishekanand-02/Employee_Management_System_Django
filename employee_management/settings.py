f0fa25eb5ed065f381e7a72a92326a9188850270
abhishekanand-02 <bgsabhishekanand02@gmail.com>
2024-09-06 11:52:00 +0530

    Updated seetings

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+%b&&_3cdf#3m%)4^$h_=pkjs^%(5pt3x@2x(m=ff+(n=&ee3f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['your-vercel-app-url.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
